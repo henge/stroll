1558e17388c402f411a67a395b7f36e254ef7558
henge <henggge@gmail.com>
2014-04-27 17:33:15 +0900

    再生位置の保持処理修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @user = User.find(current_user.id)
-    @current_order = @user.current;
+    @current_order = current_user.current;
   end
 end
